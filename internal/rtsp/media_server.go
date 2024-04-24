93c10b66b3903c705e410020ac98a1b4f3eb298b
MisterGnida <https.genius@gmail.com>
2024-04-24 16:50:01 +0300

    Removed comments

--- internal/rtsp/media_server.go
+++ internal/rtsp/media_server.go
@@ -7,85 +7,74 @@ import (
 	"github.com/bluenviron/gortsplib/v4/pkg/format"
 	"github.com/pion/rtp"
 	"log"
+	"net"
 	"sync"
 )
 
 type MediaServerHandler struct {
-	s         *gortsplib.Server
-	mutex     sync.Mutex
-	stream    *gortsplib.ServerStream
-	publisher *gortsplib.ServerSession // публикующий (текущая сессия)
+	s                *gortsplib.Server
+	mutex            sync.Mutex
+	stream           *gortsplib.ServerStream
+	publisher        *gortsplib.ServerSession
+	StreamManager    *StreamManager
+	connMicroservice net.Conn
 }
 
-// OnConnOpen called when a connection is opened.
 func (msh *MediaServerHandler) OnConnOpen(ctx *gortsplib.ServerHandlerOnConnOpenCtx) {
 	log.Printf("User Data: %v", ctx.Conn.NetConn().RemoteAddr())
 	log.Printf("conn opened")
 }
 
-// OnConnClose called when a connection is closed.
 func (msh *MediaServerHandler) OnConnClose(ctx *gortsplib.ServerHandlerOnConnCloseCtx) {
 	log.Printf("conn closed (%v)", ctx.Error)
 }
 
-// OnSessionOpen called when a session is opened.
 func (msh *MediaServerHandler) OnSessionOpen(ctx *gortsplib.ServerHandlerOnSessionOpenCtx) {
 	log.Printf("session opened")
 }
 
-// OnSessionClose called when a session is closed.
 func (msh *MediaServerHandler) OnSessionClose(ctx *gortsplib.ServerHandlerOnSessionCloseCtx) {
 	log.Printf("session closed")
 
 	msh.mutex.Lock()
 	defer msh.mutex.Unlock()
 
-	// if the session is the publisher,
-	// close the stream and disconnect any reader.
 	if msh.stream != nil && ctx.Session == msh.publisher {
 		msh.stream.Close()
 		msh.stream = nil
 	}
 }
 
-// OnDescribe called when receiving a DESCRIBE request.
 func (msh *MediaServerHandler) OnDescribe(ctx *gortsplib.ServerHandlerOnDescribeCtx) (*base.Response, *gortsplib.ServerStream, error) {
 	log.Printf("describe request")
 
 	msh.mutex.Lock()
 	defer msh.mutex.Unlock()
 
-	// no one is publishing yet
 	if msh.stream == nil {
 		return &base.Response{
 			StatusCode: base.StatusNotFound,
 		}, nil, nil
 	}
 
-	// send medias that are being published to the client
 	return &base.Response{
 		StatusCode: base.StatusOK,
 	}, msh.stream, nil
 
 }
 
-// OnAnnounce called when receiving an ANNOUNCE request.
 func (msh *MediaServerHandler) OnAnnounce(ctx *gortsplib.ServerHandlerOnAnnounceCtx) (*base.Response, error) {
 	log.Printf("announce request")
 
-	// Обработка запроса ANNOUNCE для пути /app/stream1
 	log.Println("Получен запрос ANNOUNCE на ", ctx.Request.URL)
-	// Ваша логика обработки здесь...
 
 	msh.mutex.Lock()
 	defer msh.mutex.Unlock()
-	// disconnect existing publisher
 	if msh.stream != nil {
 		msh.stream.Close()
 		msh.publisher.Close()
 	}
 
-	// create the stream and save the publisher
 	msh.stream = gortsplib.NewServerStream(msh.s, ctx.Description)
 	msh.publisher = ctx.Session
 
@@ -94,10 +83,9 @@ func (msh *MediaServerHandler) OnAnnounce(ctx *gortsplib.ServerHandlerOnAnnounce
 	}, nil
 }
 
-// OnSetup called when receiving a SETUP request.
 func (msh *MediaServerHandler) OnSetup(ctx *gortsplib.ServerHandlerOnSetupCtx) (*base.Response, *gortsplib.ServerStream, error) {
 	log.Printf("setup request")
-
+	log.Println("SETUP: ", ctx.Path)
 	// no one is publishing yet
 	if msh.stream == nil {
 		return &base.Response{
@@ -110,7 +98,6 @@ func (msh *MediaServerHandler) OnSetup(ctx *gortsplib.ServerHandlerOnSetupCtx) (
 	}, msh.stream, nil
 }
 
-// OnPlay called when receiving a PLAY request.
 func (msh *MediaServerHandler) OnPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*base.Response, error) {
 	log.Printf("play request")
 
@@ -119,13 +106,39 @@ func (msh *MediaServerHandler) OnPlay(ctx *gortsplib.ServerHandlerOnPlayCtx) (*b
 	}, nil
 }
 
-// OnRecord called when receiving a RECORD request.
+func readFromMicroservice(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		buff := make([]byte, 1024)
+		n, err := conn.Read(buff)
+		if err != nil {
+			log.Println("Error reading data from Python microservice:", err)
+			return
+		}
+
+		log.Println("Received data from Python microservice:", buff[:n])
+	}
+}
+
 func (msh *MediaServerHandler) OnRecord(ctx *gortsplib.ServerHandlerOnRecordCtx) (*base.Response, error) {
 	log.Printf("record request")
 
-	// called when receiving a RTP packet
 	ctx.Session.OnPacketRTPAny(func(medi *description.Media, forma format.Format, pkt *rtp.Packet) {
-		// route the RTP packet to all readers
+		if pkt.PayloadType == 97 {
+			_, err := msh.connMicroservice.Write(pkt.Payload)
+			if err != nil {
+				log.Println("Error during conn.Write(payload): ", err)
+			}
+		}
+		go func() {
+			buff := make([]byte, len(pkt.Payload))
+			_, err := msh.connMicroservice.Read(buff)
+			if err != nil {
+				log.Println("Error during conn.Read(buff): ", err)
+			}
+			pkt.Payload = buff
+		}()
 		msh.stream.WritePacketRTP(medi, pkt)
 	})
 
@@ -140,9 +153,12 @@ func (msh *MediaServerHandler) Server() *gortsplib.Server {
 }
 
 func Setup() *MediaServerHandler {
-	// configure the server
 	h := &MediaServerHandler{}
-
+	err := error(nil)
+	h.connMicroservice, err = net.Dial("tcp", "0.0.0.0:8081")
+	if err != nil {
+		log.Fatal("Failed to connect to Python microservice:", err)
+	}
 	h.s = &gortsplib.Server{
 		Handler:           h,
 		RTSPAddress:       ":8554",
@@ -153,8 +169,6 @@ func Setup() *MediaServerHandler {
 		MulticastRTCPPort: 8003,
 	}
 
-	// setupAndStart server and wait until a fatal error
 	log.Printf("server is ready")
-	//panic(h.s.StartAndWait())
 	return h
 }
